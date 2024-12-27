7313fcb7e5b1e892caddccc60cd99acfb31add3c
Aleksa sanyaaa_a <holmesSUQA@yandex.ru>
2024-12-28 00:53:53 +0300

    добавлены модели

--- src/main/java/ru/practicum/shareit/booking/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/Booking.java
@@ -1,7 +1,22 @@
 package ru.practicum.shareit.booking;
 
+
+import lombok.Data;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.model.User;
+
+import java.time.LocalDate;
+
 /**
  * TODO Sprint add-bookings.
  */
+
+@Data
 public class Booking {
+    private Long id;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private Item item;
+    private User booker;
+    private Status status;
 }
--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,7 +1,19 @@
 package ru.practicum.shareit.item.model;
 
+import lombok.Data;
+import ru.practicum.shareit.request.ItemRequest;
+import ru.practicum.shareit.user.model.User;
+
 /**
  * TODO Sprint add-controllers.
  */
+
+@Data
 public class Item {
+    private Long id;
+    private String name;
+    private String description;
+    private Boolean isAvailable;
+    private User owner;
+    private ItemRequest request;
 }
